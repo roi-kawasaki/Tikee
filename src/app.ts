dc8577275a025180892885bd3c69cd57bdf39789
川﨑　真人 <kawasaki@jroi.com>
2021-02-28 12:22:02 +0900

    実行しやすくした

--- src/app.ts
+++ src/app.ts
@@ -12,5 +12,10 @@ async function test(){
     console.log('⚡️ Bolt app is running!');
 };
 
+var http = require('http');
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(3000);
 
-test()
+console.log('Server running at http://127.0.0.1:3000');
